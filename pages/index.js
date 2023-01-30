669d0e066f3a5d66ea8a8e172e8bbfcb38efb0d7
William Rogers <williamrogersdev@gmail.com>
2023-01-29 18:02:55 -0600

    created header component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import Header from "./components/Header";
+
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=''></main>
+      <main className='bg-gray-100 min-h-screen'>
+        <Header/>
+      </main>
     </>
   );
 }
